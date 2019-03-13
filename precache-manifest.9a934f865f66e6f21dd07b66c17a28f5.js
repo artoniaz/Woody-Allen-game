8e1fcfd71ef36dedc2192d8452e163c8e2ce9569
artoniaz <artoni.az@gmail.com>
2019-03-13 12:01:45 +0100

    Updates

---
+++ precache-manifest.9a934f865f66e6f21dd07b66c17a28f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9107ba598dd3f92eaa09b59207bf5a0a",
+    "url": "/woody-allen-game/static/media/manhattan.9107ba59.jpg"
+  },
+  {
+    "revision": "7c983749ae5daf3915b9bada8609abcf",
+    "url": "/woody-allen-game/static/media/allenSad.7c983749.png"
+  },
+  {
+    "revision": "76f742e267c27c0ac3e23e24b0fda582",
+    "url": "/woody-allen-game/static/media/allenHappy.76f742e2.png"
+  },
+  {
+    "revision": "3a3cc85f930abb0017aa",
+    "url": "/woody-allen-game/static/js/runtime~main.3a3cc85f.js"
+  },
+  {
+    "revision": "10a85b8a7c7823625eea",
+    "url": "/woody-allen-game/static/js/main.10a85b8a.chunk.js"
+  },
+  {
+    "revision": "8bd8ca997e05d7018db3",
+    "url": "/woody-allen-game/static/js/2.8bd8ca99.chunk.js"
+  },
+  {
+    "revision": "10a85b8a7c7823625eea",
+    "url": "/woody-allen-game/static/css/main.9012fb90.chunk.css"
+  },
+  {
+    "revision": "d59db0371cc7b008b93313915e8bbdeb",
+    "url": "/woody-allen-game/index.html"
+  }
+];
