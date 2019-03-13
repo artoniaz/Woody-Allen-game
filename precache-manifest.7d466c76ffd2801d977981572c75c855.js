4edfe9510b2aa617c06bf64718d02270423c802c
artoniaz <artoni.az@gmail.com>
2019-03-13 12:10:08 +0100

    Updates

---
+++ precache-manifest.7d466c76ffd2801d977981572c75c855.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9107ba598dd3f92eaa09b59207bf5a0a",
+    "url": "/Woody-Allen-Game/static/media/manhattan.9107ba59.jpg"
+  },
+  {
+    "revision": "7c983749ae5daf3915b9bada8609abcf",
+    "url": "/Woody-Allen-Game/static/media/allenSad.7c983749.png"
+  },
+  {
+    "revision": "76f742e267c27c0ac3e23e24b0fda582",
+    "url": "/Woody-Allen-Game/static/media/allenHappy.76f742e2.png"
+  },
+  {
+    "revision": "17d579879572c3a8e105",
+    "url": "/Woody-Allen-Game/static/js/runtime~main.17d57987.js"
+  },
+  {
+    "revision": "cfcf07ab277316c5b766",
+    "url": "/Woody-Allen-Game/static/js/main.cfcf07ab.chunk.js"
+  },
+  {
+    "revision": "4594fe0e5aa41bf5ba0f",
+    "url": "/Woody-Allen-Game/static/js/2.4594fe0e.chunk.js"
+  },
+  {
+    "revision": "cfcf07ab277316c5b766",
+    "url": "/Woody-Allen-Game/static/css/main.f04deff1.chunk.css"
+  },
+  {
+    "revision": "3e07f061426a20fb6bfc5318387699e4",
+    "url": "/Woody-Allen-Game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/woody-allen-game/precache-manifest.9a934f865f66e6f21dd07b66c17a28f5.js"
+  "/Woody-Allen-Game/precache-manifest.7d466c76ffd2801d977981572c75c855.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/woody-allen-game/index.html", {
+workbox.routing.registerNavigationRoute("/Woody-Allen-Game/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
